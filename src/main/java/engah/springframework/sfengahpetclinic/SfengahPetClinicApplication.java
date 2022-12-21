1d2a2a67dd57481d2af16406805710074d08b8dc
ahmedalsayed9399 <engahmed9399@gmail.com>
2022-12-22 01:55:11 +0200

    initial project add

---
+++ src/main/java/engah/springframework/sfengahpetclinic/SfengahPetClinicApplication.java
@@ -0,0 +1,13 @@
+package engah.springframework.sfengahpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfengahPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfengahPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/engah/springframework/sfengahpetclinic/SfengahPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package engah.springframework.sfengahpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfengahPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
