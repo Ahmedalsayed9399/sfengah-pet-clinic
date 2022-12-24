ff2640b150e25d55286634e633eee8781059d857
ahmedalsayed9399 <engahmed9399@gmail.com>
2022-12-24 15:48:33 +0200

    Implement Interfaces of services. closes #5.

---
+++ pet-clinic-data/src/main/java/engah/springframework/sevices/PetService.java
@@ -0,0 +1,12 @@
+package engah.springframework.sevices;
+
+
+import engah.springframework.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
