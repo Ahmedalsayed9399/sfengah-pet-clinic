971c046904ba302f3fcd9eda253641a09d59d74c
ahmedalsayed9399 <engahmed9399@gmail.com>
2022-12-25 13:45:44 +0200

    Refactoring interfaces is done! closes #11

--- pet-clinic-data/src/main/java/engah/springframework/sevices/OwnerService.java
+++ pet-clinic-data/src/main/java/engah/springframework/sevices/OwnerService.java
@@ -2,11 +2,9 @@ package engah.springframework.sevices;
 
 import engah.springframework.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
-    Owner findBylastName(String lastname);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-Set<Owner> findAll();
+
+public interface OwnerService extends CrudService<Owner,Long>{
+    Owner findByLastName(String lastname);
+
 }
