c72d925cbda297167a0ba5fbe45d3f29aadfea3f
ahmedalsayed9399 <engahmed9399@gmail.com>
2022-12-26 17:28:06 +0200

    update map impl to manage setting of id. Closes #23

--- pet-clinic-web/src/main/java/engah/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/engah/springframework/bootstrap/DataLoader.java
@@ -20,23 +20,19 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1=new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("EngAhmed");
         owner1.setLastName("Elsayed");
         ownerService.save(owner1);
         Owner owner2=new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("EngMohamed");
         owner2.setLastName("Elsayed");
         ownerService.save(owner2);
         System.out.println("load owners is done!....");
         Vet vet1=new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
         Vet vet2=new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         vetService.save(vet2);
