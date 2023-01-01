b961969b59a9d400a3d37e979e83a56e0017797d
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-01 20:39:34 +0200

    done added petType to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/engah/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/engah/springframework/bootstrap/DataLoader.java
@@ -2,7 +2,9 @@ package engah.springframework.bootstrap;
 
 import engah.springframework.model.Owner;
 import engah.springframework.model.Vet;
+import engah.springframework.model.petType;
 import engah.springframework.sevices.OwnerService;
+import engah.springframework.sevices.PetTypeService;
 import engah.springframework.sevices.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,23 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
  private final OwnerService ownerService;
   private  final VetService vetService;
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+  private final PetTypeService petTypeService;
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        petType dog=new petType();
+        dog.setName("Dog");
+        petType savedDogPetType=petTypeService.save(dog);
+        petType cat=new petType();
+        dog.setName("Cat");
+        petType savedCatPetType=petTypeService.save(cat);
+
         Owner owner1=new Owner();
         owner1.setFirstName("EngAhmed");
         owner1.setLastName("Elsayed");
