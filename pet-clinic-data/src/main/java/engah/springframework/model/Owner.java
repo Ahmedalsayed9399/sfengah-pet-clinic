3de201c322e0a39fad39f2eed1a4c3035bcb545e
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-01 19:46:15 +0200

    done added contact info to owner. Closes #28

--- pet-clinic-data/src/main/java/engah/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/engah/springframework/model/Owner.java
@@ -3,7 +3,34 @@ package engah.springframework.model;
 import java.util.Set;
 
 public class Owner extends Person{
+    private String address;
+    private String Telephone;
+    private String city;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
