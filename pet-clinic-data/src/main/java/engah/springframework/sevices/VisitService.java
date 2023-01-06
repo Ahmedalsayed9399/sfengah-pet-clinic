74ce92d673f2d8642ff49218e08d098ee82ee78c
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-06 06:13:19 +0200

    done adding visit map service. Closes #46

---
+++ pet-clinic-data/src/main/java/engah/springframework/sevices/VisitService.java
@@ -0,0 +1,6 @@
+package engah.springframework.sevices;
+
+import engah.springframework.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+}
---
+++ pet-clinic-data/src/main/java/engah/springframework/sevices/map/VisitMapService.java
@@ -0,0 +1,49 @@
+package engah.springframework.sevices.map;
+
+import engah.springframework.model.Visit;
+import engah.springframework.sevices.PetService;
+import engah.springframework.sevices.PetTypeService;
+import engah.springframework.sevices.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long>implements VisitService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public VisitMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet()==null||visit.getPet().getOwner()==null||visit.getPet().getId()==null
+        ||visit.getPet().getOwner().getId()==null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+super.deleteById(id);
+    }
+}
