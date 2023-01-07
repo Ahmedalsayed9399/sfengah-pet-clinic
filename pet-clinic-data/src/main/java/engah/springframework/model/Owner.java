758fe2f194ede7fdcdf3e9ffdad3e1f2eebecd43
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-07 02:11:00 +0200

    done lombok project. Closes #51

--- pet-clinic-data/src/main/java/engah/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/engah/springframework/model/Owner.java
@@ -1,12 +1,30 @@
 package engah.springframework.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
+@Setter
+@Getter
+@NoArgsConstructor
+
+
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+    @Builder
+    public Owner(Long id,String firstName,String lastName,String address,
+                 String telephone, String city, Set<Pet> pets) {
+        super(id,firstName,lastName);
+        this.address = address;
+        Telephone = telephone;
+        this.city = city;
+        this.pets = pets;
+    }
+
+
+
     @Column(name = "address")
     private String address;
     @Column(name = "telephone")
@@ -16,35 +34,5 @@ public class Owner extends Person{
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
     private Set<Pet> pets=new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return Telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        Telephone = telephone;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
