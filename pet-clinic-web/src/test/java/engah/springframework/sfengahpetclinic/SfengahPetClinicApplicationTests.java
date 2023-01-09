5f32635dc0c87dd032c6b3ab56182e64013c0c18
ahmedalsayed9399 <engahmed9399@gmail.com>
2023-01-09 05:28:10 +0200

    done upgrading junit. Closes #4

--- pet-clinic-web/src/test/java/engah/springframework/sfengahpetclinic/SfengahPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/engah/springframework/sfengahpetclinic/SfengahPetClinicApplicationTests.java
@@ -1,8 +1,11 @@
 package engah.springframework.sfengahpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfengahPetClinicApplicationTests {
 
